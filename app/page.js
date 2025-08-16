d035cf7b22e936c77ca554d1f70837664a37d923
xrwong00 <22003580@siswa.um.edu.my>
2025-08-16 13:17:01 +0800

    Add components

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <div>Hi there! 😊</div>
-  );
+  return <div>Hi there! 😊
+<br/>
+
+<Button>Hello</Button>
+
+    </div>;
 }
