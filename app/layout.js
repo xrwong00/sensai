81f88d9304ab6bac9251f9446fe5ce905d0ac244
xrwong00 <22003580@siswa.um.edu.my>
2025-08-16 13:29:38 +0800

    Update components

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,23 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            {/* footer */}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>© 2025 Sensai. All rights reserved.</p>
+              </div>
+            </footer>
+
+          </ThemeProvider>
       </body>
     </html>
   );
